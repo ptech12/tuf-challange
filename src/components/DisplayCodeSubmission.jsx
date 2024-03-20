2f3e3fbbdaff3fee8dea0d50a29dc58ad65011ec
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-21 00:45:26 +0530

    display data in table

--- src/components/DisplayCodeSubmission.jsx
+++ src/components/DisplayCodeSubmission.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import "../App.css"
+import React, { forwardRef } from "react";
+import "../App.css";
 
-const DisplayCodeSubmission = ({ data }) => {
-  // console.log(data);
+const DisplayCodeSubmission = ({ data }, ref) => {
+  console.log(data);
+  const displayTimeStamp = (timestamp) => {
+    const date = new Date(timestamp);
+
+    // Format the date and time using options for locale and date/time style
+    const formattedDate = date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: "Asia/Calcutta",
+    });
+
+    return formattedDate;
+  };
+  // return (<></>)
   return (
-    <table>
-      <tr>
-        <th>ID</th>
-        <th>Username</th>
-        <th>Language</th>
-        <th>Stdin</th>
-        <th>Source Code</th>
-        <th>Time Stamp</th>
-      </tr>
-    </table>
-  )
-}
+    <section ref={ref} className="page2">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Language</th>
+            <th>Stdin</th>
+            <th>Source Code</th>
+            <th>Time Stamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data.id}</td>
+            <td>{data.username}</td>
+            <td>{data.lang}</td>
+            <td>{data.stdin}</td>
+            <td>{data.src_code.length <= 100 ? data.src_code : `${(data.src_code).slice(0, 99)+'...'}`}</td>
+            <td>{displayTimeStamp(data.created)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
-export default DisplayCodeSubmission
+export default forwardRef(DisplayCodeSubmission);
